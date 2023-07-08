b31d32a1809a2ab29410f826a0c2292b196bc473
Martin Löper <martin.loeper@nesto-software.de>
2023-07-08 12:52:52 +0200

    feat: add package git-credential-manager from utybo

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My personal NUR repository";
+  description = "Personal NUR repository by mloeper";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
