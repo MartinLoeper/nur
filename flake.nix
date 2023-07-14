df169eb55f044a97795243e4b6301db604478375
Martin Löper <martin.loeper@nesto-software.de>
2023-07-14 02:58:24 +0200

    refactor: remove unused dotnet overlay§

--- flake.nix
+++ flake.nix
@@ -17,9 +17,6 @@
       legacyPackages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { 
           inherit system;
-          overlays = [ 
-            (import ./overlays/dotnet.nix { inherit nixpkgs; })
-          ];
         };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
