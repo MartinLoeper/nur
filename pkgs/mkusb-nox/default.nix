feb9e69bc16e51727498ef585e1fe504c7b8bcd9
Martin Löper <martin.loeper@nesto-software.de>
2023-09-20 03:13:30 +0200

    fix: fetcher for nox

--- pkgs/mkusb-nox/default.nix
+++ pkgs/mkusb-nox/default.nix
@@ -4,26 +4,33 @@ stdenv.mkDerivation {
   pname = "mkusb-nox";
   version = "23.1.2";
 
-  src = fetchTarball
+  src = fetchFromGitHub
     {
-      url = "https://github.com/sudodus/tarballs/raw/master/mkusb-nox.tar.xz";
-      sha256 = "sha256:0kyli7xgdvb6wg5g48hjgp5yl3i8wkrvnwhhav955bi5sqs4xs8r";
+      owner = "sudodus";
+      repo = "tarballs";
+      rev = "93b43c208e902d0f8064b3b0abf461765b273a53";
+      sha256 = "sha256-FcI/GKLjhIN0YxXNxE6bagGyQ7o9SwtHfIonrXc4EkE=";
     };
 
   buildInputs = with pkgs; [
     pv
   ];
 
+  nativeBuildInputs = with pkgs; [
+    xz
+  ];
+
   unpackPhase = ":";
   installPhase = ''
     install -d $out/bin
-    cp $src $out/bin/mkusb-nox
+    tar -xf $src/mkusb-nox.tar.xz
+    cp mkusb-nox $out/bin/mkusb-nox
   '';
 
   meta = with lib; {
     homepage = "https://help.ubuntu.com/community/mkusb/v7";
     description = "Copy an ISO file to a USB device";
-    license = licenses.gpl2Plus;
+    license = licenses.gpl3Only;
     platforms = [ "x86_64-linux" ];
     mainProgram = "mkusb-nox";
   };
