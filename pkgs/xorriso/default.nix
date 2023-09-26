df62851a6463183ee04fc16473e10846a6bcaa2f
Martin Löper <martin.loeper@nesto-software.de>
2023-09-26 14:04:01 +0200

    fix: keep xorriso version from nixpkgs

--- pkgs/xorriso/default.nix
+++ pkgs/xorriso/default.nix
@@ -2,11 +2,6 @@
 pkgs.xorriso.overrideAttrs (old: {
   version = "${old.version}-patched";
 
-  src = fetchurl {
-    url = "https://www.gnu.org/software/xorriso/xorriso-${old.version}.tar.gz";
-    sha256 = "sha256-hnV3w4f2tKmjIk60Qd7Y+xY432y8Bg+NGh5dAPMY9QI=";
-  };
-
   preInstall = ''
     substituteInPlace "xorriso-dd-target/xorriso-dd-target" \
       --replace "xdt_init ||" "xdt_lsblk_cmd=${pkgs.util-linux}/bin/lsblk;xdt_dd_cmd=${pkgs.coreutils}/bin/dd;xdt_umount_cmd=${pkgs.umount}/bin/umount ||"
