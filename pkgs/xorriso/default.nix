e4305a22eccadf681e7b28492e2d1461858795cb
Martin Löper <martin.loeper@nesto-software.de>
2023-09-26 14:06:28 +0200

    fix: do not change the original version

--- pkgs/xorriso/default.nix
+++ pkgs/xorriso/default.nix
@@ -1,7 +1,5 @@
 { lib, stdenv, pkgs, fetchurl, ... }:
 pkgs.xorriso.overrideAttrs (old: {
-  version = "${old.version}-patched";
-
   preInstall = ''
     substituteInPlace "xorriso-dd-target/xorriso-dd-target" \
       --replace "xdt_init ||" "xdt_lsblk_cmd=${pkgs.util-linux}/bin/lsblk;xdt_dd_cmd=${pkgs.coreutils}/bin/dd;xdt_umount_cmd=${pkgs.umount}/bin/umount ||"
