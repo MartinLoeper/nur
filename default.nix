31d635b2e106a3424a48ed3cf96fc212bb9dd3e3
Martin Löper <martin.loeper@nesto-software.de>
2023-10-25 16:06:59 +0200

    init: dynobase

--- default.nix
+++ default.nix
@@ -23,4 +23,5 @@ rec {
   mkusb-sedd = pkgs.callPackage ./pkgs/mkusb-sedd { };
   xorriso = pkgs.callPackage ./pkgs/xorriso { };
   aws-iot-securetunneling-localproxy = pkgs.callPackage ./pkgs/aws-iot-securetunneling-localproxy { };
+  dynobase = pkgs.callPackage ./pkgs/dynobase { };
 }
